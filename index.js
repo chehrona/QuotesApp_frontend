8d3c40c6daf1dc2207edf14f350e2c3542804b03
Chehronai Fozil <54600544+chehrona@users.noreply.github.com>
2022-05-11 20:25:02 -0400

    frontend

---
+++ index.js
@@ -0,0 +1,52 @@
+$( document ).ready(function() {
+
+    // Input: list of dictionary
+    // Output: void
+    let update = function(data) {
+        if (data.length == 1) {
+            $(".main").html(data[0].entry);
+        }
+        if (data.length == 2) {
+            $(".main").html(data[data.length - 1].entry)
+            $(".quote3").html(data[data.length - 2].entry);
+        }
+        if (data.length == 3) {
+            $(".main").html(data[data.length - 1].entry)
+            $(".quote3").html(data[data.length - 2].entry);
+            $(".quote2").html(data[data.length - 3].entry);
+        }
+        if (data.length >= 4) {
+            $(".main").html(data[data.length - 1].entry)
+            $(".quote3").html(data[data.length - 2].entry);
+            $(".quote2").html(data[data.length - 3].entry);
+            $(".quote1").html(data[data.length - 4].entry);
+
+        }
+    }
+    $.get("http://127.0.0.1:8000/retrieve/", function (data) {
+        console.log(data);
+        update(data);
+    });
+
+    let adding = function(data) {
+        let submission = $(".entryForm").val();
+         $.post("http://127.0.0.1:8000/send/", {
+             'entry': submission
+         }, function (data) {
+             console.log(data);
+             update(data);
+            $(".entryForm").val('')
+         });
+        // alert(submission);
+    }
+    $(".button").click(function() {
+        adding()
+    })
+
+    $(".entryForm").on("keydown", function (e) {
+        if(e.key === 'Enter') {
+            adding();
+        }
+    });
+
+});
